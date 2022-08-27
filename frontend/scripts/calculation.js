0a0b6e95799252403c673c995e3acdd7a47cdf93
PhilAnggara <bawoleangga03@gmail.com>
2022-08-27 19:49:58 +0800

    search

---
+++ frontend/scripts/calculation.js
@@ -0,0 +1,45 @@
+function euclidean(lat1, lon1, lat2, lon2) {
+  const d = Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lon1 - lon2, 2)) * 100000;
+  document.getElementById('euclidean').value = normalize(d);
+
+  return d;
+}
+
+function spherical(lat1, lon1, lat2, lon2) {
+  const R = 6371e3;
+  const φ1 = (lat1 * Math.PI) / 180;
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180
+  const d = Math.acos(Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)) * R;
+  document.getElementById('spherical').value = normalize(d);
+
+  return d;
+}
+
+function haversine(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of earth in KM
+  var dLat = (lat2 - lat1) * Math.PI / 180;
+  var dLon = (lon2 - lon1) * Math.PI / 180;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c * 1000; // Distance in m
+  document.getElementById('haversine').value = normalize(d);
+
+  return d;
+}
+
+function normalize(x) {
+  if (x >= 1000) {
+    return (x / 1000).toFixed(3) + ' km';
+  } else {
+    return x.toFixed(1) + ' m';
+  }
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  euclidean(lat1, lon1, lat2, lon2);
+  spherical(lat1, lon1, lat2, lon2);
+  haversine(lat1, lon1, lat2, lon2);
+}
--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -4,33 +4,9 @@ AOS.init({
   duration: 600
 });
 
-const toastTriggerA = document.getElementById('copyA')
-const toastLiveExampleA = document.getElementById('liveToastA')
-if (toastTriggerA) {
-  toastTriggerA.addEventListener('click', () => {
-    const toast = new bootstrap.Toast(toastLiveExampleA)
-
-    toast.show()
-  })
-}
-const toastTriggerB = document.getElementById('copyB')
-const toastLiveExampleB = document.getElementById('liveToastB')
-if (toastTriggerB) {
-  toastTriggerB.addEventListener('click', () => {
-    const toast = new bootstrap.Toast(toastLiveExampleB)
-
-    toast.show()
-  })
-}
-
-
-// Hybrid: s,h;
-// Satellite: s;
-// Streets: m;
-// Terrain: p;
 var map = L.map('map').setView([1.4822795012983179, 124.86507303747182], 13);
 L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
-  maxZoom: 19,
+  maxZoom: 20,
   attribution: '© PhilAnggara',
   subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
@@ -38,7 +14,6 @@ L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
 var aIcon = L.icon({
   iconUrl: 'frontend/images/marker-icon-a.png',
   shadowUrl: 'frontend/images/marker-shadow.png',
-
   iconSize:     [33, 42],
   shadowSize:   [50, 64],
   iconAnchor:   [16, 42],
@@ -48,7 +23,6 @@ var aIcon = L.icon({
 var bIcon = L.icon({
   iconUrl: 'frontend/images/marker-icon-b.png',
   shadowUrl: 'frontend/images/marker-shadow.png',
-
   iconSize:     [33, 42],
   shadowSize:   [50, 64],
   iconAnchor:   [16, 42],
@@ -56,107 +30,185 @@ var bIcon = L.icon({
   popupAnchor:  [-3, -76]
 });
 
-var markerA = L.marker([1.5049181818799706, 124.87268109746775], {
+var lokasiA = {
+  lat: 1.5049181818799706,
+  lng: 124.87268109746775
+};
+var lokasiB = {
+  lat: 1.4907184564345943,
+  lng: 124.83914510504695
+};
+
+var markerA = L.marker(lokasiA, {
   icon: aIcon,
   draggable: true,
 }).addTo(map);
-var markerB = L.marker([1.4907184564345943, 124.83914510504695], {
+var markerB = L.marker(lokasiB, {
   icon: bIcon,
   draggable: true,
 }).addTo(map);
-var polygon = L.polygon([
-  [markerA.getLatLng().lat, markerA.getLatLng().lng],
-  [markerB.getLatLng().lat, markerB.getLatLng().lng]
-]).addTo(map);
+var polygon = L.polygon([lokasiA, lokasiB]).addTo(map);
 
-var euclideanResult = euclidean(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng);
-var sphericalResult = spherical(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng);
-var haversineResult = haversine(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng);
 
-showResult(euclideanResult, sphericalResult, haversineResult);
+var inputA = document.getElementById("inputA");
+var inputB = document.getElementById("inputB");
+getUserLocation();
+setLocationName(lokasiA.lat, lokasiA.lng, inputA);
+setLocationName(lokasiB.lat, lokasiB.lng, inputB);
+calculateDistance(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng)
 
-document.getElementById('latA').value = markerA.getLatLng().lat;
-document.getElementById('lngA').value = markerA.getLatLng().lng;
-document.getElementById('latB').value = markerB.getLatLng().lat;
-document.getElementById('lngB').value = markerB.getLatLng().lng;
-
-markerA.on('move', function(e) {
-  // console.log('Marker A : ',markerA.getLatLng().lat, markerA.getLatLng().lng);
-  document.getElementById('latA').value = markerA.getLatLng().lat;
-  document.getElementById('lngA').value = markerA.getLatLng().lng;
-  polygon.setLatLngs([
-    [markerA.getLatLng().lat, markerA.getLatLng().lng],
-    [markerB.getLatLng().lat, markerB.getLatLng().lng]
-  ]);
-  showResult(
-    euclidean(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng),
-    spherical(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng),
-    haversine(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng)
-  );
-});
-
-markerB.on('move', function(e) {
-  // console.log('Marker B : ',markerB.getLatLng().lat, markerB.getLatLng().lng);
-  document.getElementById('latB').value = markerB.getLatLng().lat;
-  document.getElementById('lngB').value = markerB.getLatLng().lng;
-  polygon.setLatLngs([
-    [markerA.getLatLng().lat, markerA.getLatLng().lng],
-    [markerB.getLatLng().lat, markerB.getLatLng().lng]
-  ]);
-  showResult(
-    euclidean(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng),
-    spherical(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng),
-    haversine(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng)
-  );
-});
-
-console.log(euclidean(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng));
-console.log(spherical(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng));
-console.log(haversine(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng));
-
-function euclidean(lat1, lon1, lat2, lon2) {
-  return Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lon1 - lon2, 2)) * 100000;
-}
-
-function spherical(lat1, lon1, lat2, lon2) {
-  const R = 6371e3;
-  const φ1 = (lat1 * Math.PI) / 180;
-  const φ2 = (lat2 * Math.PI) / 180;
-  const Δλ = ((lon2 - lon1) * Math.PI) / 180
-  const d = Math.acos(Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)) * R;
-
-  return d;
-}
-
-// calculate distance between two points in latitude and longitude using haversine formula
-function haversine(lat1, lon1, lat2, lon2) {
-  var R = 6371; // Radius of earth in KM
-  var dLat = (lat2 - lat1) * Math.PI / 180;
-  var dLon = (lon2 - lon1) * Math.PI / 180;
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-          Math.sin(dLon/2) * Math.sin(dLon/2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c;
-  return d * 1000; // meters
-}
-
-function showResult(euclideanResult, sphericalResult, haversineResult) {
-  document.getElementById('euclidean').value = normalize(euclideanResult);
-  document.getElementById('spherical').value = normalize(sphericalResult);
-  document.getElementById('haversine').value = normalize(haversineResult);
-}
-
-function normalize(x) {
-  if (x >= 1000) {
-    return (x / 1000).toFixed(3) + ' km';
+function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError, {
+      enableHighAccuracy: true,
+    });
   } else {
-    return x.toFixed(1) + ' m';
+    console.log("Geolokasi tidak didukung oleh browser ini.");
+  }
+}
+function showPosition(position) {
+  setLocationName(position.coords.latitude, position.coords.longitude, inputA);
+  markerA.setLatLng([position.coords.latitude, position.coords.longitude]);
+  map.flyToBounds([markerA.getLatLng(), markerB.getLatLng()]);
+  polygon.setLatLngs([markerA.getLatLng(), markerB.getLatLng()]);
+  calculateDistance(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng)
+}
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      console.log("Pengguna menolak permintaan Geolokasi.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log("Informasi lokasi tidak tersedia.");
+      break;
+    case error.TIMEOUT:
+      console.log("Permintaan untuk mendapatkan lokasi pengguna kedaluwarsa.");
+      break;
+    case error.UNKNOWN_ERROR:
+      console.log("Terjadi kesalahan yang tidak diketahui.");
+      break;
   }
 }
 
-function copyToClipboard(latId, lngId) {
-  var lat = document.getElementById(latId);
-  var lng = document.getElementById(lngId);
-  navigator.clipboard.writeText(lat.value + ',' + lng.value);
+markerA.on('drag', function(e) {
+  inputA.value = 'Mendapatkan nama lokasi...';
+  inputA.classList.add('text-muted');
+  inputA.setAttribute('readonly', 'readonly');
+  polygon.setLatLngs([markerA.getLatLng(), markerB.getLatLng()]);
+});
+markerB.on('drag', function(e) {
+  inputB.value = 'Mendapatkan nama lokasi...';
+  inputB.classList.add('text-muted');
+  inputB.setAttribute('readonly', 'readonly');
+  polygon.setLatLngs([markerA.getLatLng(), markerB.getLatLng()]);
+});
+
+markerA.on('dragend', function(e) {
+  setLocationName(markerA.getLatLng().lat, markerA.getLatLng().lng, inputA);
+  calculateDistance(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng);
+});
+markerB.on('dragend', function(e) {
+  setLocationName(markerB.getLatLng().lat, markerB.getLatLng().lng, inputB);
+  calculateDistance(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng);
+});
+
+function setLocationName(lat, lng, input) {
+  input.removeAttribute('readonly');
+  var mapboxAccessToken = 'pk.eyJ1IjoiamVuaWZlcmRhbWFyIiwiYSI6ImNrcTZrYXY2cDFzeXIyb280eTBndTAwMHMifQ.AAdI5SToEU4ITSvEqPcGIA';
+  var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+lng+','+lat+'.json?reverseMode=distance&language=id&access_token='+mapboxAccessToken;
+  fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  })
+  .then(data => {
+    input.value = data.features[0].text;
+    input.classList.remove('text-muted');
+  })
+  .catch(error => {
+    console.log(error);
+  });
 }
+
+// calculate distance between two points in km
+
+function searchLocation(inputId, suggestionId) {
+  
+  const input = document.getElementById(inputId);
+  const suggestion = document.getElementById(suggestionId);
+  suggestion.classList.remove('d-none');
+  var mapboxAccessToken = 'pk.eyJ1IjoiamVuaWZlcmRhbWFyIiwiYSI6ImNrcTZrYXY2cDFzeXIyb280eTBndTAwMHMifQ.AAdI5SToEU4ITSvEqPcGIA';
+  var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+input.value+'.json?country=id&limit=10&language=id&access_token='+mapboxAccessToken;
+  fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  })
+  .then(data => {
+    showSuggestion(data, input, suggestion);
+    console.log(keyword);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+const showSuggestion = (data, input, suggestion) => {
+  console.clear();
+  if (input.value !== '') {
+    if (data.features.length > 0) {
+      console.log(data.features);
+      data.features.forEach((element, index) => {
+        console.log(index);
+        console.log(element.place_name);
+        console.log(element.center[1]+', '+element.center[0]);
+        console.log(element.text);
+      });
+      const html = data.features.map(element => `
+        <button
+          type="button"
+          class="list-group-item list-group-item-action"
+          onclick="selectLocation(${element.center[1]}, ${element.center[0]}, '${element.text}', '${input.id}')"
+        >
+          ${element.place_name}
+        </button>
+      `).join('');
+      suggestion.innerHTML = html;
+    } else {
+      console.log('Lokasi tidak ditemukan.');
+      suggestion.innerHTML ='<button type="button" class="list-group-item list-group-item-action text-center" disabled>Lokasi tidak ditemukan.</button>';
+    }
+  } else {
+    suggestion.innerHTML = '';
+  }
+}
+
+function selectLocation(lat, lng, text, inputId) {
+  const input = document.getElementById(inputId);
+  input.value = text;
+  if (inputId === 'inputA') {
+    markerA.setLatLng([lat, lng]);
+  } else {
+    markerB.setLatLng([lat, lng]);
+  }
+  map.flyToBounds([markerA.getLatLng(), markerB.getLatLng()]);
+  polygon.setLatLngs([markerA.getLatLng(), markerB.getLatLng()]);
+  calculateDistance(markerA.getLatLng().lat, markerA.getLatLng().lng, markerB.getLatLng().lat, markerB.getLatLng().lng)
+}
+
+document.addEventListener('click', function handleClickOutsideBox(event) {
+  // const box = document.getElementById('box');
+
+  // if (!box.contains(event.target)) {
+  //   box.style.display = 'none';
+  // }
+  console.log('click');
+  document.getElementById('suggestionA').classList.add('d-none');
+  document.getElementById('suggestionB').classList.add('d-none');
+});
